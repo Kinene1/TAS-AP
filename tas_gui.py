d8fa1387a3c4dd00fec3dddde9c9be68b7d8a5ef
Tonny Kinene <kinenetonny@gmail.com>
2026-05-18 09:33:28 +0800

    Update tas_gui.py

--- tas_gui.py
+++ tas_gui.py
@@ -9,6 +9,32 @@ import queue
 import time
 import webbrowser
 
+# Tonny Kinene (Tonny.Kinene@dpird.wa.gov.au)
+
+#-----------------------------------------------------------------------
+# DPIRD DIagnostics and Laboratory Services
+# Sustainability and Biosecurity 
+# Department of Primary Industries and Regional Development
+# 31 Cedric Street, Stirling WA 6021
+# ----------------------------------------------------------------------
+
+# Copyright (c) 2026 Tonny Kinene
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+#------------------------------------------------------------------------
+
+
 # ------------------------------
 # Theme
 # ------------------------------
